090f8e066151c760c2de073a6a71437ec070dcb0
ASG <gulisikaynur@gmail.com>
2022-08-16 10:34:54 -0500

    Added more work

--- src/work1/HelloClass.java
+++ src/work1/HelloClass.java
@@ -4,7 +4,7 @@ public class HelloClass {
     public static void main(String[] args) {
         System.out.println("Work1 started");
         System.out.println("Created Git Repository");
-
+        System.out.println("Added more work");
 
 
 
