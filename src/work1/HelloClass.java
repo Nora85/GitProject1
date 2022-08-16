2da15abab745ecf77a824f91ff8e83b3a008f1b1
ASG <gulisikaynur@gmail.com>
2022-08-16 10:31:46 -0500

    initial commit

--- src/work1/HelloClass.java
+++ src/work1/HelloClass.java
@@ -3,7 +3,7 @@ package work1;
 public class HelloClass {
     public static void main(String[] args) {
         System.out.println("Work1 started");
-
+        System.out.println("Created Git Repository");
 
 
 
